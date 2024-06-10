3affcbee50f81242876da46eb261e751a44865b7
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-06-10 16:26:50 +0200

    update_null
    still doesn't work...

--- Includes/_Footer.php
+++ Includes/_Footer.php
@@ -2,5 +2,5 @@
     
 
     <p>Copyright © 2024. Tous droits réservés.</p>
-    <link rel="stylesheet" href="./Css/index.css?v=<?php echo time(); ?>">
+    
 </footer>
--- Includes/_Header.php
+++ Includes/_Header.php
@@ -3,5 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bientôt 3000 km</title>
     <p>Conduite accompagnée Max : en route vers le A !</p>
-    <link rel="stylesheet" href="./Css/index.css?v=<?php echo time(); ?>">
+    
 </header>
